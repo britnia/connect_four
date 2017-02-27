187e0b42c28a81226e03c893c002929d0b2a1b9f
Britni <BAlexander@Britnis-MacBook-Pro.local>
2017-02-26 19:21:15 -0500

    bug fix and refactor for row and column checker

--- app/controllers/concerns/game_rules.rb
+++ app/controllers/concerns/game_rules.rb
@@ -9,26 +9,13 @@ module Concerns::GameRules
 
   def connect_four_by_column? board
     board.each do |column|
-      count = 0
-      last = column.first
-      while !column.empty? do
-        current = column.shift
-        next if current == 'e' || current == 0
-        if current == last
-          count += 1
-          return true if count == 4
-        else
-          count = 1
-        end
-        last = current
-      end
+      return true if (column.reverse.join.include?('bbbb') || column.reverse.join.include?('rrrr'))
     end
     false
   end
 
   def connect_four_by_row? board
-    flip_board = board.transpose
-    connect_four_by_column? flip_board
+    connect_four_by_column? board.transpose
   end
 
   def connect_four_by_diagonal? board
